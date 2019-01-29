dcf6332e9eb8ce905644e079a06924ba4234345b
Robert Zyla <rebok415gmail.com>
2019-01-29 11:50:14 +0100

    implementacja przyciskow, planszy, logiki gry

---
+++ KolkoKrzyzyk/GraStatus.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KolkoKrzyzyk
+{
+    //pobieranie status aktualnego gry
+    class GraStatus
+    {
+        public bool koniecGry;
+        public string zwyciezca;
+
+        public GraStatus()
+        {
+            koniecGry = false;
+            zwyciezca = null;
+        }
+
+        //zwrocenie status gry
+        public GraStatus(bool koniecGry, string zwyciezca)
+        {
+            this.koniecGry = koniecGry;
+            this.zwyciezca = zwyciezca;
+        }
+
+        //zakonczenie gry
+        public void ustawkoniecGry(bool koniecGry)
+        {
+            this.koniecGry = koniecGry;
+        }
+        
+        //zakonczenie gry
+        public bool jesliKoniecGry()
+        {
+            return koniecGry;
+        }
+
+        //ustawienie zwyciezcy
+        public void ustawZwyciezce(string zwyciezca)
+        {
+            this.zwyciezca = zwyciezca;
+        }
+
+        //pobieranie zwyciezcy
+        public string pobierzZwyciezce()
+        {
+            return zwyciezca;
+        }
+        
+    }
+}
--- KolkoKrzyzyk/MainWindow.xaml.cs
+++ KolkoKrzyzyk/MainWindow.xaml.cs
@@ -22,6 +22,8 @@ namespace KolkoKrzyzyk
     {
         private List<Button> graPrzyciski;
 
+        public bool AKCJA { get; private set; }
+
         public MainWindow()
         {
             InitializeComponent();
@@ -40,5 +42,140 @@ namespace KolkoKrzyzyk
             graPrzyciski.Add(wiersz3pole2);
             graPrzyciski.Add(wiersz3pole3);
         }
+
+        //przyciski do gry na planszy
+        private void startGry_Przycisk(object sender, RoutedEventArgs e)
+        {
+
+            Button nacisnieciePrzycisku = (Button)sender;
+            if (AKCJA)
+            {
+                nacisnieciePrzycisku.Content = Stale.X_ZNAK;
+                nacisnieciePrzycisku.IsEnabled = false;
+                AKCJA = false;
+            }
+            else
+            {
+                nacisnieciePrzycisku.Content = Stale.O_ZNAK;
+                nacisnieciePrzycisku.IsEnabled = false;
+                AKCJA = true;
+            }
+
+            if (sprawdzStatusGry())
+            {
+                return;
+            }
+            sprawdzStatusGry();
+        }
+
+
+        //sprawdzam kto wygral gre
+        private bool sprawdzStatusGry()
+        {
+            GraStatus status = sprawdzPoziom();
+            if (status.jesliKoniecGry())
+            {
+                     
+                if (status.zwyciezca == Stale.O_ZNAK)
+                    MessageBox.Show("gracz 2 wygrywa!");
+                else
+                    MessageBox.Show("gracz 1 wygrywa!");
+                return true;
+            }
+
+            //sprawdzanie poziomu
+            status = sprawdzPoziom();
+
+            //sprawdzanie statusu gry
+            if (status.jesliKoniecGry())
+            {
+                zakonczGre();
+                aktualizujStatus(status);
+
+                if (status.zwyciezca == Stale.O_ZNAK)
+                    MessageBox.Show("gracz 2 wygrywa!");
+                else
+                    MessageBox.Show("gracz 1 wygrywa!");
+                return true;
+            }
+
+            status = sprawdzPoziom();
+            if (status.jesliKoniecGry())
+            {
+                  
+                if (status.zwyciezca == Stale.O_ZNAK)
+                    MessageBox.Show("gracz 2 wygrywa!");
+                else
+                    MessageBox.Show("gracz 1 wygrywa!");
+                return true;
+            }
+            return false;
+        }
+
+        //zakonczenie gry
+        private void zakonczGre()
+        {
+            foreach (Button przycisk in graPrzyciski)
+            {
+                przycisk.IsEnabled = false;
+            }
+        }
+
+        //aktualizuje status gry
+        private void aktualizujStatus(GraStatus status)
+        {
+            if (status.jesliKoniecGry())
+            {
+                if (status.pobierzZwyciezce().Equals(Stale.X_ZNAK))
+                {
+                    int obecnyWynik = Convert.ToInt32(wygrywaX.Content);
+                    wygrywaX.Content = "" + (obecnyWynik + 1);
+                }
+                else if (status.pobierzZwyciezce().Equals(Stale.O_ZNAK))
+                {
+                    int obecnyWynik = Convert.ToInt32(wygrywa0.Content);
+                    wygrywa0.Content = "" + (obecnyWynik + 1);
+                }
+            }
+        }
+
+        //sprawdzam wynik poziomu
+        private GraStatus sprawdzPoziom()
+        {
+            bool koniecGry = false;
+            string zwyciezca = "";
+            
+            //sprawdzanie co w danym momencie jest nacisniete
+            if ( wiersz1pole1.Content.Equals( wiersz1pole2.Content)
+                &&  wiersz1pole1.Content.Equals( wiersz1pole3.Content)
+                &&  wiersz1pole2.Content.Equals( wiersz1pole3.Content)
+                && !wiersz1pole1.Content.Equals(""))
+            {
+                
+                koniecGry = true;
+                zwyciezca = Convert.ToString( wiersz1pole1.Content);
+            }
+            else if ( wiersz2pole1.Content.Equals( wiersz2pole2.Content)
+                    &&  wiersz2pole1.Content.Equals( wiersz2pole3.Content)
+                    &&  wiersz2pole2.Content.Equals( wiersz2pole3.Content)
+                    && !wiersz2pole1.Content.Equals(""))
+            {
+                
+                koniecGry = true;
+                zwyciezca = Convert.ToString( wiersz2pole1.Content);
+            }
+            else if ( wiersz3pole1.Content.Equals( wiersz3pole2.Content)
+                    &&  wiersz3pole1.Content.Equals( wiersz3pole3.Content)
+                    &&  wiersz3pole2.Content.Equals( wiersz3pole3.Content)
+                    && !wiersz3pole1.Content.Equals(""))
+            {
+                
+                koniecGry = true;
+                zwyciezca = Convert.ToString( wiersz3pole1.Content);
+            }
+            return new GraStatus(koniecGry, zwyciezca);
+        }
+
+
     }
 }
